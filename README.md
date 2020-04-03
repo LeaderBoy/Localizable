1af9af36f1d77b68071adf7aabe56485a6ae1819
iMac Company <748603349@qq.com>
2020-04-03 12:06:13 +0800

    1.initial

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # Localizable
 
-A description of this package.
+[Code From](https://medium.com/@mendibarouk/enhance-your-localized-capabilities-on-your-ios-applications-d3ba17138077)
+
+### How to get the localized string?
+just 
+```swift
+.food.local /// Food or 食物
+```
+
+#### Example 
+
+```swift
+enum Tag : String {
+    case traffic
+    case food
+    case cosmetics
+}
+
+extension Tag : Localizable {
+    var key: String {
+        return "tag_" + self.rawValue
+    }
+
+    var value: String {
+        return self.rawValue
+    }
+}
+```
+
+in Localizable.strings you need the following:
+
+English Version
+
+```swift
+"tag_traffic" = "Traffic";
+"tag_food" = "Food";
+"tag_cosmetics" = "Cosmetics";
+```
+
+Chinese Version
+
+```swift
+"tag_traffic" = "交通";
+"tag_food" = "餐饮";
+"tag_cosmetics" = "化妆品";
+```
+
+
--- Sources/Localizable/Localizable.swift
+++ Sources/Localizable/Localizable.swift
@@ -1,3 +1,70 @@
-struct Localizable {
-    var text = "Hello, World!"
+
+import Foundation
+
+/// **Configurate Example** :
+//    enum Tag : String {
+//        case traffic
+//        case food
+//        case cosmetics
+//    }
+//
+//    extension Tag : Localizable {
+//        var key: String {
+//            return "tag_" + self.rawValue
+//        }
+//
+//        var value: String {
+//            return self.rawValue
+//        }
+//    }
+
+/// in Localizable.strings you need the following:
+//    "tag_traffic" = "Traffic";
+//    "tag_food" = "Food";
+//    "tag_cosmetics" = "Cosmetics";
+
+/// **Use Case Example**:
+///     when you need a localized string, you only need to call
+///     `.traffic.local`
+
+
+/// A locallized protocol
+/// code from https://medium.com/@mendibarouk/enhance-your-localized-capabilities-on-your-ios-applications-d3ba17138077
+public protocol Localizable {
+    var key : String { get }
+    var value : String { get }
+    var comment : String { get }
+    var bundle : Bundle { get }
+    var arguments : [CVarArg] { get }
+    var tableName : String? { get }
 }
+
+extension Localizable {
+    var value : String {
+        return ""
+    }
+    var comment : String {
+        return ""
+    }
+    var bundle : Bundle {
+        return Bundle.main
+    }
+    var arguments : [CVarArg] {
+       return []
+    }
+    var tableName : String? {
+        return nil
+    }
+}
+
+extension Localizable {
+    fileprivate var defaultLocalizedString : String {
+        return NSLocalizedString(key, tableName: tableName, bundle: bundle, value: value, comment: comment)
+    }
+    
+    /// Localized string
+    public var local : String {
+        return String(format: defaultLocalizedString, arguments)
+    }
+}
+
